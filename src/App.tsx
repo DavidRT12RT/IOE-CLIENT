11bbe4766e1cc87186d6d70ff23296dfb9b8a3e7
David AM <davidarcos@hotmail.com>
2023-12-29 16:41:43 -0600

    Firts Commit , Proyect Three Doned

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+//Pages
+import Inventarios from './pages/Inventarios';
+
+// TODO: Make React routes
+
+function App() {
+
+	return (
+		<>
+		 	<header></header>
+			<main>
+				<Inventarios/>
+    		</main>
+			<footer></footer>
+		</>
+  	);
+
+}
+
+export default App
---
+++ src/views/inventarios/Inventarios.tsx
@@ -0,0 +1,24 @@
+import "../styles/inventarios.css";
+
+const Inventarios = () => {
+
+    return (
+        <section className="Inventarios">
+
+            <div className="Finder">
+                <input 
+                    type="text"
+                    placeholder="Find something..."
+                />
+            </div>
+
+            <div className="Results">
+                <p>algo</p>
+            </div>
+
+        </section>
+    );
+
+}
+
+export default Inventarios
